65d647effd00342854555d010e80a9600cab792e
GAudrey <gilmantaudrey.ag@gmail.com>
2020-12-15 15:19:52 +0100

    regex doesn't support lists, tables, codes and multiquot yet

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
 const textarea = document.getElementById('text-markdown');
 const preview = document.getElementById('preview-area');
+let text;
 
-textarea.addEventListener('keydown', () =>{
-    preview.textContent = textarea.value;
+function toMarkdown(textTrans) {
+    text = textTrans
+        // titles
+        .replace(/^# (.*$)/gim, '<h1>$1</h1>')
+        .replace(/^#{2} (.*$)/gim, '<h2>$1</h2>')
+        .replace(/^#{3} (.*$)/gim, '<h3>$1</h3>')
+        .replace(/^#{4} (.*$)/gim, '<h4>$1</h4>')
+        .replace(/^#{5} (.*$)/gim, '<h5>$1</h5>')
+        .replace(/^#{6} (.*$)/gim, '<h6>$1</h6>')
+        // hr
+        .replace(/^_{3}/gim, '<hr />')
+        .replace(/^-{3}/gim, '<hr />')
+        .replace(/^\*{3}/gim, '<hr />')
+        // blockquote
+        .replace(/^\> (.*$)/gim, '<blockquote>$1</blockquote>')
+        // bold and italic
+        .replace(/\*{2}(.*)\*{2}/gim, '<strong>$1</strong>')
+        .replace(/\_{2}(.*)\_{2}/gim, '<strong>$1</strong>')
+        .replace(/\*(.*)\*/gim, '<em>$1</em>')
+        .replace(/\_(.*)\_/gim, '<em>$1</em>')
+        .replace(/\~{2}(.*)\~{2}/gim, '<s>$1</s>')
+        // lists
+            // .replace(/^\* (.*$)/gim, '<ul><li>$1</li></ul>')
+            // .replace(/^\- (.*$)/gim, '<ul><li>$1</li></ul>')
+            // .replace(/^\+ (.*$)/gim, '<ul><li>$1</li></ul>')
+            // .replace(/^\d\. (.*$)/gim, '<ol><li>$1</li></ol>')
+        // code
+            // to do
+        // links and co
+		.replace(/\!\[(.*?)\]\((.*?)\)/gim, "<img alt='$1' src='$2' />")
+        .replace(/\[(.*?)\]\((.*?)\)/gim, "<a href='$2'>$1</a>")
+        
+        .replace(/\n/gim, '<br />')
+
+	return text.trim()
+}
+
+textarea.addEventListener('keyup', () =>{
+    if ()
+    let textTrans = textarea.value;
+    toMarkdown(textTrans)
+    preview.innerHTML = text;
 })
